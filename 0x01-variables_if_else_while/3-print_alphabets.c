e17e9d05a1faa5015d1780062032f45f96dbb640
Janespot <janejunell777@gmail.com>
2022-09-12 12:04:50 -0700

    updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,53 +1,27 @@
 #include <stdio.h>
 
 
-
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	char l;
 
-		char l;
-
-
-
-			l = 'a';
-
-				while (l <= 'z')
-
-						{
-
-									putchar(l);
-
-											l++;
-
-												}
-
-					l = 'A';
-
-						while (l <= 'Z')
-
-								{
-
-											putchar(l);
-
-													l++;
-
-														}
-
-							putchar('\n');
-
-								return (0);
-
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	l = 'A';
+	while (l <= 'Z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
 }
